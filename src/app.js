181aa6b8eb230b5c5466bf83c39c364b325e3b24
achiewe <mamukashviliachi706@gmail.com>
2023-08-13 17:45:16 +0400

    add bodyParser and cors in app.js

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 import express from "express";
 import connect from "./database/mongo.js";
 import dotenv from "dotenv";
+import bodyParser from "body-parser";
+import cors from "cors";
 import { getAllDevjobs } from "./controllers/project-controller.js";
 
 dotenv.config();
@@ -8,6 +10,9 @@ dotenv.config();
 connect();
 
 const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
 
 app.get("/", (req, res) => {
   return res.status(200).json({ message: "app works" });
